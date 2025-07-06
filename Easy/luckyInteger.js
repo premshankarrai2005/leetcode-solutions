c0af4614e9155e7c61c3d7b60cb08255b8d1cb04
Prem Shankar Rai <premshankarrai2005@gmail.com>
2025-07-06 21:09:54 +0530

    Add Lucky Integer Solution

---
+++ Easy/luckyInteger.js
@@ -0,0 +1,31 @@
+function findLucky(arr) {
+  const map = new Map();
+
+  // Count frequencies
+  for (let num of arr) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  let result = -1;
+
+  // Check lucky numbers
+  for (let [num, freq] of map) {
+    if (num === freq) {
+      result = Math.max(result, num); // it return which is greater result or num    
+    }
+  }
+
+  return result;
+
+}
+
+console.log(findLucky([1, 2, 2, 3, 3, 3])); //3
+console.log(findLucky([2,2,2,3,3])); //-1
+console.log(findLucky([2,2,3,4])); //2
+
+
+
+
+
+
+
